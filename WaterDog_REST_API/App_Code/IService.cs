20948073c679961b8ef864fbf0e50fd3eb8a67da
Austin-Holland1010 <austinpholland@yahoo.com>
2023-07-09 11:15:43 -0700

    Configured the REST API for future dev
    - Created a new project for creating the REST services
    - Updated the Service.svc file with appropriate config settings
    - Wrote two test services to verify the API works correctly

---
+++ WaterDog_REST_API/App_Code/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	[WebGet (UriTemplate = "PI", ResponseFormat = WebMessageFormat.Json)]
+	double PiValue();
+
+	[OperationContract]
+	[WebGet(UriTemplate = "add2?x={x}&y={y}", ResponseFormat = WebMessageFormat.Json)]
+	int addition(int x, int y);
+	//http://localhost:55250/Service.svc/add2?x=4&y=7
+}
