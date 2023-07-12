98f9bb3f15b54b9f94587c738ef7ec4ea569e01a
Austin-Holland1010 <austinpholland@yahoo.com>
2023-07-12 16:19:49 -0700

    Added a table to view customers
    - Created a useState array to hold customer data
    - The data is populated upon first accessing the application via an API call
    - Added a table to view customers that uses the useState (passed in as a prop) and maps the values to rows in the table.
    - Bootstrap was added to the project to assist in UI creation.

--- waterdog_react_app/src/components/ViewCustomers.js
+++ waterdog_react_app/src/components/ViewCustomers.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Table from 'react-bootstrap/Table'
 
-function ViewCustomers() {
+function ViewCustomers(props) {
     return (
         <div>
-            View
+            <Table striped bordered hover variant="dark" size="sm">
+                <tr>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Address</th>
+                    <th>Route</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                </tr>
+                
+                {props.customers.map((customer, index) =>
+                    <tr data-index={index}>
+                        <td>{customer.firstname}</td>
+                        <td>{customer.lastname}</td>
+                        <td>{customer.address}</td>
+                        <td>{customer.route}</td>
+                        <td>{customer.phone}</td>
+                        <td>{customer.email}</td>
+                    </tr>
+                )}
+            </Table>
         </div>
     )
 }
